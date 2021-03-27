03c6591eca8032db460a0a202a1f79d37f0595bf
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2021-03-27 12:04:01 +0100

    add mapkit and save button

---
+++ MyToDoList/MapController.swift
@@ -0,0 +1,30 @@
+//
+//  MapController.swift
+//  MyToDoList
+//
+//  Created by Viktor Kuzmanov on 3/27/21.
+//  Copyright © 2021 ASN GROUP LLC. All rights reserved.
+//
+
+import UIKit
+
+class MapController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyToDoList/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  MyToDoList
+//
+//  Created by Viktor Kuzmanov on 3/24/21.
+//  Copyright © 2021 ASN GROUP LLC. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
